67282f3bcc885e9096c3f6c09dd5c815161c793b
Tom Elliott <tomelliottnz@gmail.com>
2020-01-08 09:07:27 +1300

    package documentation now using roxygen2

---
+++ R/iNZightMR-package.R
@@ -0,0 +1,20 @@
+#' iNZightMR: Multiple Response Data Analysis
+#'
+#' The iNZightMR package provides a suite of functions which can be used
+#' in the analysis of multiple response data. It is used in the
+#' iNZight software package.
+#'
+#' \tabular{ll}{
+#'   Package: \tab iNZightMR\cr
+#'   Type: \tab Package\cr
+#'   Version: \tab 2.0\cr
+#'   Date: \tab 2014-09-19\cr
+#'   License: \tab GPL-2\cr
+#' }
+#'
+#' @author Junjie Zeng, Tom Elliott
+#' @keywords iNZight multiple response
+#'
+#' @doctype package
+#' @name iNZightMR-package
+NULL
